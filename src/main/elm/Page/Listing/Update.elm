e58626f4d4e1f38a4d1d7da377ac0da622f24d2e
Eike Kettner <eike.kettner@posteo.de>
2020-04-01 22:38:53 +0200

    Remove Debug calls in elm

--- src/main/elm/Page/Listing/Update.elm
+++ src/main/elm/Page/Listing/Update.elm
@@ -2,21 +2,19 @@ module Page.Listing.Update exposing (update)
 
 import Page.Listing.Data exposing (..)
 
-update: Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         AllScripts (Ok scripts) ->
-            ( {model | scripts = scripts }
+            ( { model | scripts = scripts }
             , Cmd.none
             )
 
-        AllScripts (Err err) ->
-            let
-                _ = Debug.log "Error getting scripts:" err
-            in
+        AllScripts (Err _) ->
             ( model, Cmd.none )
 
         SetCategory c ->
-            ( {model | category = c }
+            ( { model | category = c }
             , Cmd.none
             )
